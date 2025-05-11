dcd4368efd1f3b3a4a5f97d68e8e69eeb1fdefdd
Leofer Dela Cruz <Leoferma099@gmail.com>
2025-05-11 14:46:02 +0800

    update env and web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/check-log', function () {
+    return response()->file(storage_path('logs/laravel.log'));
+});
