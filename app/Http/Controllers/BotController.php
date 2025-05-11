e34e4efd41febe8da4416f978ea20c6354076d7a
Leofer Dela Cruz <Leoferma099@gmail.com>
2025-05-11 11:51:22 +0800

    modification     -   BotController     -   env     -   render yaml

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -13,6 +13,11 @@ class BotController extends Controller
         // Predefined responses schema
         $responses = [
             [
+                'type' => 'emergency',
+                'keywords' => ['help'],
+                'reply' => "Of course, we are here to help you. What is your emergency?"
+            ],
+            [
                 'type' => 'greeting',
                 'keywords' => ['hi', 'hello', 'hey'],
                 'reply' => "Hello! I'm AskDoctor AI. How can I assist you today?"
@@ -30,7 +35,42 @@ class BotController extends Controller
             [
                 'type' => 'diagnosis_advice',
                 'keywords' => ['pain', 'hurt'],
-                'reply' => "It sounds serious. I recommend visiting the nearest hospital as soon as you can."
+                'reply' => "It sounds serious. I recommend visiting the nearest hospital as soon as you can. Where are you located?"
+            ],
+            [
+                'type' => 'location_1',
+                'keywords' => ['aurora'],
+                'reply' => "Great! Here is the nearest hospital. Premiere General Hospital, Baler and the emergency contact is +63-946-068-0000"
+            ],
+                        [
+                'type' => 'location_2',
+                'keywords' => ['bataan'],
+                'reply' => "Great! Here is the nearest hospital. Bataan General Hospital and Medical Center, Balanga City and the emergency contact is (047) 237-9771"
+            ],
+                        [
+                'type' => 'location_3',
+                'keywords' => ['bulacan'],
+                'reply' => "Great! Here is the nearest hospital. Bulacan Medical Center, Malolos City Center and the emergency contact is  (044) 794-1526"
+            ],
+                        [
+                'type' => 'location_4',
+                'keywords' => ['tarlac'],
+                'reply' => "Great! Here is the nearest hospital. Tarlac Provincial Hospital, Tarlac City and the emergency contact is (045) 982-6885"
+            ],
+                        [
+                'type' => 'location_5',
+                'keywords' => ['pampanga'],
+                'reply' => "Great! Here is the nearest hospital. PRI Medical Center, San Fernando City and the emergency contact is 0917-180-8886"
+            ],
+                        [
+                'type' => 'location_6',
+                'keywords' => ['nueva ecija'],
+                'reply' => "Great! Here is the nearest hospital. GoodSam Medical Center, Cabanatuan City and the emergency contact is (044) 951-8888"
+            ],
+                        [
+                'type' => 'location_7',
+                'keywords' => ['zambales'],
+                'reply' => "Great! Here is the nearest hospital. President Ramon Magsaysay Memorial Hospital, Iba and the emergency contact is 0906-340-9108"
             ],
             [
                 'type' => 'thank_you',
